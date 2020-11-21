4b0cdae9220d8979c86f970509510125373fb550
Jason Manuel <jama.indo@hotmail.com>
2020-11-21 11:51:21 -0700

    Make checks fall and destroyable by clicking

---
+++ Assets/Scripts/DestroyOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DestroyOnClick : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        // Destroy the gameObject after clicking on it
+        Destroy(gameObject);
+    }
+}
