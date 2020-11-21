77512d4ea4f9b0ff18cd25b294902aad18341f27
Ajide49 <ausman3@asu.edu>
2020-11-21 01:56:42 -0700

    add social distance micro game

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private bool isDragging;
+    private GameObject timer;
+    bool hasGameEnded;
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if(TimeLeft.timeLeft <= 0){
+            hasGameEnded = true;
+        }
+        else{
+            hasGameEnded = false;
+        }
+
+        if(!hasGameEnded){
+            if(isDragging) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+             }
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectDistance.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDistance : MonoBehaviour
+{
+    public GameObject Blob1;
+    public GameObject Blob2;
+    public GameObject Blob3;
+    public GameObject Blob4;
+    public Sprite RedSprite, GreenSprite, YellowSprite;
+   
+    public float BlobDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BlobDistance = Vector3.Distance(Blob1.transform.position, Blob2.transform.position);
+        if(BlobDistance < 5){
+            Blob1.GetComponent<SpriteRenderer>().sprite = RedSprite;
+            Blob2.GetComponent<SpriteRenderer>().sprite = RedSprite;
+        }
+        else if(BlobDistance > 5 && BlobDistance < 10){
+            Blob1.GetComponent<SpriteRenderer>().sprite = YellowSprite;
+            Blob2.GetComponent<SpriteRenderer>().sprite = YellowSprite;
+        }
+        else{
+            Blob1.GetComponent<SpriteRenderer>().sprite = GreenSprite;
+            Blob2.GetComponent<SpriteRenderer>().sprite = GreenSprite;
+        }
+    }
+}
